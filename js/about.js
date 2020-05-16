383638bc63850894851ce56194031f5102f08551
py-thony <byamungutony@gmail.com>
2020-05-16 17:03:46 +0200

    created a mentoring request form and styled it

--- js/about.js
+++ js/about.js
@@ -26,6 +26,17 @@ $(function() {
 });
 
 
+var quotes = ["“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” – Martin Fowler",
+"“First, solve the problem. Then, write the code.” – John Johnson",
+"“Experience is the name everyone gives to their mistakes.” – Oscar Wilde",
+"“Knowledge is power.” – Francis Bacon",
+"“Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code.” – Dan Salomon",
+"“ Code is like humor. When you have to explain it, it’s bad.” – Cory House",
+"“Fix the cause, not the symptom.” – Steve Maguire",
+"Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live. - Martin Golding",
+]
+
+
 
 
 
